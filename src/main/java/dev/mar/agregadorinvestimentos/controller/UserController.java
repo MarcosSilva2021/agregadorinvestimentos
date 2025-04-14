f730f6b4e5eb203ba384d9c6ea7596a03544d49a
Marcosdsj <mfsilva5585@gmail.com>
2025-04-14 16:20:45 -0300

    update

--- src/main/java/dev/mar/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/dev/mar/agregadorinvestimentos/controller/UserController.java
@@ -1,11 +1,31 @@
 package dev.mar.agregadorinvestimentos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.mar.agregadorinvestimentos.entity.User;
+import dev.mar.agregadorinvestimentos.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
+    private UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto ){
+        var userId = userService.createUser(createUserDto);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
+        //
+        return null;
+    }
 }
--- src/main/java/dev/mar/agregadorinvestimentos/service/UserService.java
+++ src/main/java/dev/mar/agregadorinvestimentos/service/UserService.java
@@ -1,10 +1,13 @@
 package dev.mar.agregadorinvestimentos.service;
 
+import dev.mar.agregadorinvestimentos.controller.CreateUserDto;
 import dev.mar.agregadorinvestimentos.entity.User;
 import dev.mar.agregadorinvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -12,7 +15,22 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto){
+        // converter DTO -> Entity
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null);
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
     }
 
     public List<User> listUser(){
