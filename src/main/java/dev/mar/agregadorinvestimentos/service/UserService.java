1adb9d82ec0a1e14a5ba8882620c3e00a074d230
Marcosdsj <mfsilva5585@gmail.com>
2025-04-21 21:14:10 -0300

    create method listUsers

--- src/main/java/dev/mar/agregadorinvestimentos/service/UserService.java
+++ src/main/java/dev/mar/agregadorinvestimentos/service/UserService.java
@@ -42,4 +42,8 @@ public class UserService {
         return userRepository.findById(UUID.fromString(userId));
     }
 
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
 }
