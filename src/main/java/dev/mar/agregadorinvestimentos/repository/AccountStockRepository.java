41726c6830a562e038737863a7b3196a50aff6e3
Marcosdsj <mfsilva5585@gmail.com>
2025-05-08 15:51:37 -0300

    update Id

--- src/main/java/dev/mar/agregadorinvestimentos/repository/AccountStockRepository.java
+++ src/main/java/dev/mar/agregadorinvestimentos/repository/AccountStockRepository.java
@@ -1,11 +1,12 @@
 package dev.mar.agregadorinvestimentos.repository;
 
 import dev.mar.agregadorinvestimentos.entity.AccountStock;
+import dev.mar.agregadorinvestimentos.entity.AccountStockId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AccountStockRepository extends JpaRepository<AccountStock, UUID> {
+public interface AccountStockRepository extends JpaRepository<AccountStock, AccountStockId> {
 }
