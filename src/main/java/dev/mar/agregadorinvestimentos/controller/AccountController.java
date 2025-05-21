3ee1528fe3f1f10069eb8fbe799b126674d48b0e
Marcosdsj <mfsilva5585@gmail.com>
2025-05-21 16:21:00 -0300

    create method postmapping associateStock

--- src/main/java/dev/mar/agregadorinvestimentos/controller/AccountController.java
+++ src/main/java/dev/mar/agregadorinvestimentos/controller/AccountController.java
@@ -1,8 +1,10 @@
 package dev.mar.agregadorinvestimentos.controller;
 
+import dev.mar.agregadorinvestimentos.controller.dto.AssociateAccountStockDto;
+import dev.mar.agregadorinvestimentos.controller.dto.CreateAccountDto;
 import dev.mar.agregadorinvestimentos.service.AccountService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/accounts")
@@ -12,6 +14,13 @@ public class AccountController {
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @PostMapping("/{accountsId}/stocks")
+    public ResponseEntity<Void> associateStock(@PathVariable("accountsId") String accountsId,
+                                               @RequestBody AssociateAccountStockDto assAccountStockDto) {
+        accountService.associateStock(accountsId, assAccountStockDto);
+        return ResponseEntity.ok().build();
     }
 
 
